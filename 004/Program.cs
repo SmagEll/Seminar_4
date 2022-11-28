9ca9b0211abc6a40ae04e57ab46cec28082c481c
Elena.Smagina <esmagina17@gmail.com>
2022-11-28 16:20:12 +0500

    004

---
+++ 004/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int [] GetArray(int [] array)
+{
+
+for (int i = 0; i <array.Length; i++)
+{
+array[i] = new Random().Next(0,2);
+}
+
+return array;
+
+}
+int [] array = new int [8];
+GetArray(array);
+
+Console.Write(String.Join(",", array));
+
